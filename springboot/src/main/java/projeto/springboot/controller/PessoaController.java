9ce193e155575decfc56b4f9f4192303f80c8bac
Ssxun3r <gabrielfillip@gmail.com>
2023-03-04 09:59:15 -0300

    Excluindo dados do cadastro com Thymeleaf e Spring Boot

--- springboot/src/main/java/projeto/springboot/controller/PessoaController.java
+++ springboot/src/main/java/projeto/springboot/controller/PessoaController.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -55,5 +56,18 @@ public class PessoaController {
 		modelAndView.addObject("pessoaobj",pessoa.get());
 		return modelAndView;
 	}
+	
+	@GetMapping("/removerpessoa/{idpessoa}")
+	public ModelAndView excluir(@PathVariable("idpessoa") Long idpessoa) {
+		
+		pessoaRepository.deleteById(idpessoa);
+		
+		ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
+		modelAndView.addObject("pessoas", pessoaRepository.findAll());
+		modelAndView.addObject("pessoaobj", new Pessoa());
+		return modelAndView;
+		
+	}
+	
 	
 }
