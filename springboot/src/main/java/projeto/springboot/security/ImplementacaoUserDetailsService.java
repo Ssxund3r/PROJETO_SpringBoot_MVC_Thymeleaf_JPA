1e90e1303f441fe50c9ef3b7421aa6d51be99f78
Ssxun3r <gabrielfillip@gmail.com>
2023-03-14 21:53:56 -0300

    Criando controle de acessos e perfil (ROLES) Spring Security

--- springboot/src/main/java/projeto/springboot/security/ImplementacaoUserDetailsService.java
+++ springboot/src/main/java/projeto/springboot/security/ImplementacaoUserDetailsService.java
@@ -1,18 +1,19 @@
 package projeto.springboot.security;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import projeto.springboot.model.Usuario;
 import projeto.springboot.repository.UsuarioRepository;
 
-@Configuration
-@EnableWebSecurity
-@SuppressWarnings("deprecation")
+@Service
+@Transactional
 public class ImplementacaoUserDetailsService implements UserDetailsService {
 	
 	@Autowired
@@ -26,11 +27,7 @@ public class ImplementacaoUserDetailsService implements UserDetailsService {
 			throw new UsernameNotFoundException("Dados incorretos... Tente novamente!");
 		}
 		
-		return usuario;
-	}
-	
-	
-	
-	
-	
+		return new User(usuario.getLogin(), usuario.getPassword(), 
+				usuario.isEnabled(), true, true, true, usuario.getAuthorities());
+	}	
 }
--- springboot/src/main/java/projeto/springboot/security/WebConfigSecurity.java
+++ springboot/src/main/java/projeto/springboot/security/WebConfigSecurity.java
@@ -25,6 +25,7 @@ public class WebConfigSecurity extends WebSecurityConfigurerAdapter {
 		http.csrf().disable() // Desativa as configurações padrão de memória.
 				.authorizeRequests() // Pertimir restringir acessos
 				.antMatchers(HttpMethod.GET, "/").permitAll() // Qualquer usuário acessa a pagina inicial
+				.antMatchers(HttpMethod.GET, "/cadastropessoa").hasAnyRole("ADMIN")
 				.anyRequest().authenticated().and().formLogin().permitAll() // permite qualquer usuário
 				.and().logout() // Mapeia URL de Logout e invalida usuário autenticado
 				.logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
